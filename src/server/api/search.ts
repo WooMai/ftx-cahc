25757fdab5fd6fd561164405cfb47b6c456ccc62
Arush <arush@users.noreply.github.com>
2024-03-02 17:04:21 +0100

    removed getSome in favor of default state

--- src/server/api/search.ts
+++ src/server/api/search.ts
@@ -3,25 +3,6 @@
 import axios, { type AxiosResponse } from "axios";
 import type { ISearchRequest } from "@/app/models/Search.model";
 
-export const getSome: () => Promise<AxiosResponse> = async function () {
-    return await axios.post(`https://fastapi-production-c0c6.up.railway.app/search`, {
-        conditions: [
-            {
-                "name": "GBP",
-                "min_balance": 387489,
-                "max_balance": 387490
-            },
-            {
-                "name": "BTC",
-                "min_balance": 15.7,
-                "max_balance": 16
-            }
-        ],
-        "page": 1,
-        "page_size": 10
-    })
-}
-
 export const searchWithConditions = async function (queryData: ISearchRequest) {
     return await axios.post(`https://fastapi-production-c0c6.up.railway.app/search`, queryData)
 }
