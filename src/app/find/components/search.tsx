b96a455cc6f6aa0f384f836a4af1adb05aa1e01c
Arush <arush@users.noreply.github.com>
2024-03-22 13:41:02 +0100

    pagination working great

--- src/app/find/components/search.tsx
+++ src/app/find/components/search.tsx
@@ -4,7 +4,13 @@ import { searchWithConditions } from "@/server/api/search";
 import { useQuery } from "@tanstack/react-query";
 import ClaimItem from "./claim-item";
 import { Claim, type ClaimSchema } from "@/app/models/Claim.model";
-import { type Key, type SetStateAction, useState, Suspense } from "react";
+import {
+  type Key,
+  type SetStateAction,
+  useState,
+  useEffect,
+  Suspense,
+} from "react";
 import SearchForm from "@/app/_components/search-form";
 import {
   type ISearchRequest,
@@ -18,12 +24,26 @@ export function Search({ assets }: { assets: { name: string }[] }) {
   const [searchConditions, setSearchConditions] =
     useState(defaultSearchPayload);
 
+  // In the Search component
+  const [currentPage, setCurrentPage] = useState(defaultSearchPayload.page);
+  useEffect(() => {
+    setCurrentPage(1); // Reset currentPage to 1 whenever searchConditions change
+  }, [searchConditions]);
+
   const performSearch: (
     arg0: SetStateAction<ISearchRequest>,
   ) => Promise<void> = async (
     searchRequest: SetStateAction<ISearchRequest>,
   ) => {
     setSearchConditions(searchRequest);
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const handlePreviousPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
   };
 
   return (
@@ -34,20 +54,34 @@ export function Search({ assets }: { assets: { name: string }[] }) {
         performSearch={performSearch}
       />
 
-      <SearchResults searchConditions={searchConditions} />
+      <SearchResults
+        searchConditions={searchConditions}
+        currentPage={currentPage}
+        onNextPage={handleNextPage}
+        onPreviousPage={handlePreviousPage}
+      />
     </>
   );
 }
 
 export function SearchResults({
   searchConditions,
+  currentPage,
+  onNextPage,
+  onPreviousPage,
 }: {
   searchConditions: ISearchRequest;
+  currentPage: number;
+  onNextPage: () => void;
+  onPreviousPage: () => void;
 }) {
   const { data, isLoading, error } = useQuery({
-    queryKey: ["searchResults", JSON.stringify(searchConditions)],
+    queryKey: [
+      "searchResults",
+      JSON.stringify({ ...searchConditions, page: currentPage }),
+    ],
     queryFn: (): Promise<AxiosResponse<ISearchResponse>> =>
-      searchWithConditions(searchConditions),
+      searchWithConditions({ ...searchConditions, page: currentPage }),
   });
 
   if (isLoading) {
@@ -99,8 +133,17 @@ export function SearchResults({
     <>
       <div>
         <p className="my-3 w-full pr-3 text-right text-xs italic text-stone-500">
-          Showing <span className="font-medium">{searchConditions.page}</span>{" "}
-          to <span className="font-medium">{searchConditions.page_size}</span>{" "}
+          Showing{" "}
+          <span className="font-medium">
+            {(currentPage - 1) * searchConditions.page_size + 1}
+          </span>{" "}
+          to{" "}
+          <span className="font-medium">
+            {Math.min(
+              currentPage * searchConditions.page_size,
+              searchResponse.total_records,
+            )}
+          </span>{" "}
           of <span className="font-medium">{searchResponse.total_records}</span>{" "}
           results
         </p>
@@ -133,11 +176,15 @@ export function SearchResults({
           <div className="hidden sm:block">
             <p className="text-sm text-stone-400">
               Showing{" "}
-              <span className="font-medium">{searchConditions.page}</span> to{" "}
               <span className="font-medium">
-                {searchResponse.total_records < searchConditions.page_size
-                  ? searchResponse.total_records
-                  : searchConditions.page_size}
+                {(currentPage - 1) * searchConditions.page_size + 1}
+              </span>{" "}
+              to{" "}
+              <span className="font-medium">
+                {Math.min(
+                  currentPage * searchConditions.page_size,
+                  searchResponse.total_records,
+                )}
               </span>{" "}
               of{" "}
               <span className="font-medium">
@@ -147,18 +194,18 @@ export function SearchResults({
             </p>
           </div>
           <div className="flex flex-1 justify-between sm:justify-end">
-            <a
-              href="#"
+            <button
+              onClick={onPreviousPage}
               className="relative inline-flex items-center rounded-md bg-stone-800 px-3 py-2 text-sm text-stone-400 ring-1 ring-inset ring-stone-700 hover:bg-stone-700/80 focus-visible:outline-offset-0"
             >
               Previous
-            </a>
-            <a
-              href="#"
+            </button>
+            <button
+              onClick={onNextPage}
               className="relative ml-3 inline-flex items-center rounded-md bg-stone-800 px-3 py-2 text-sm text-stone-400 ring-1 ring-inset ring-stone-700 hover:bg-stone-700/80 focus-visible:outline-offset-0"
             >
               Next
-            </a>
+            </button>
           </div>
         </nav>
       </ul>
